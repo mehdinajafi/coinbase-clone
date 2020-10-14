9874b41082dd8759b4560d51f79069a78167d750
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-10-15 00:12:26 +0330

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,29 @@ const Header = () => {
   return (
     <header>
       <Navbar />
+
+      <div className="w-screen bg-primary">
+        <div className="container text-center px-8 py-16">
+          <span className="text-2xl md:text-5xl text-white font-bold">
+            Buy and sell cryptocurrency
+          </span>
+          <h4 className="py-4 text-white">
+            Coinbase is the easiest place to buy, sell, and manage your
+            cryptocurrency portfolio.
+          </h4>
+          <form className="flex justify-center flex-col md:flex-row">
+            <input
+              className="my-3 md:my-0 p-4 w-full md:w-auto bg-gray-200 appearance-none border-2 border-gray-200 rounded mr-2 leading-tight focus:outline-none"
+              id="inline-full-name"
+              type="email"
+              placeholder="Email address"
+            />
+            <button className="bg-green-500 hover:bg-green-600 text-white p-4 rounded">
+              Get started
+            </button>
+          </form>
+        </div>
+      </div>
     </header>
   )
 }
